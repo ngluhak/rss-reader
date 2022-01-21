c63b9fd2bd3b3eacc64d35aeefc9437731351214
NinaG <ninaa.gluhak@gmail.com>
2022-01-21 13:04:37 +0100

    add button  for follows

--- resources/views/Follows/index.blade.php
+++ resources/views/Follows/index.blade.php
@@ -5,11 +5,16 @@
 <table class="table table-hover">
     <tr class="table-dark">
         <th>user_id</th>
+        <th>action</th>
 
         @foreach ($follows as $follow)
             <tbody>
                 <tr >
                     <th>{{ $follow->user->name ?? '' }}</th>
+                    <th>
+                        <button onclick="javascript:alert('not implemented yet')" type="button" class="btn btn-outline-success">Add</button>
+
+                    </th>
 
                 </tr>
             </tbody>
